a029f4ae98fa1da26cb2f40f54a79d61f78c7f24
OjaswiPande <101341557+OjaswiPande@users.noreply.github.com>
2022-10-17 21:54:37 +0530

    #3 Problem solved using java
    OJASWI PANDE
    CCOEW
    COMP SY
    THANK YOU

---
+++ Question_3.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main
+{
+    static String removeDuplicate(char str[], int n)
+    {
+        // Used as index in the modified string
+        int index = 0;
+
+        // Traverse through all characters
+        for (int i = 0; i < n; i++)
+        {
+
+            // Check if str[i] is present before it
+            int j;
+            for (j = 0; j < i; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    break;
+                }
+            }
+
+            // If not present, then add it to
+            // result.
+            if (j == i)
+            {
+                str[index++] = str[i];
+            }
+        }
+        return String.valueOf(Arrays.copyOf(str, index));
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        
+        char str[] = "abcc".toCharArray();
+        int n = str.length;
+        System.out.println(removeDuplicate(str, n));
+    }
+}
