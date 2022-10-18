ba54e29d140069a897e8f21905f9681f509e986c
Divyesh Agarwal <a.divyesh@iitg.ac.in>
2022-10-19 02:23:26 +0530

    Create Q27.py

---
+++ Q27.py
@@ -0,0 +1,21 @@
+r = 3   #rows
+c = 3   #columns
+import sys
+
+def minC(blocks, m, n):
+	if (n < 0 or m < 0):
+		return sys.maxsize
+	elif (m == 0 and n == 0):
+		return blocks[m][n]
+	else:
+		return blocks[m][n] + min(minC(blocks, m-1, n), minC(blocks, m, n-1) )
+def min(x, y):
+	if (x < y):
+		return x 
+	else:
+		return y 
+blocks= [ [1, 2, 1],   #This is the input matrix
+		[1, 4, 1],
+		[2, 3, 1] ]
+
+print(minC(blocks, 2, 2))
