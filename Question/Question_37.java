4f076dc3489ddb09e97a5ea95df6e9537dffbf8d
Joydeep Mallick <94801952+JoydeepMallick@users.noreply.github.com>
2022-10-22 08:19:37 +0530

    Update and rename Q37.java to Question/Question_37.java

--- Question/Question_37.java
+++ Question/Question_37.java
@@ -1,3 +1,62 @@
+/*
+Question 37
+___________
+
+Given a grid of R rows and C columns each cell in the grid is either 0 or 1.
+
+A segment is a nonempty sequence of consecutive cells such that all cells are in the same row or the same column. We define the length of a segment as number of cells in the sequence.
+
+A segment is called "good" if all the cells in the segment contain only 1s.
+
+An "L-shape" is defined as an unordered pair of segments, which has all the following properties:
+
+Each of the segments must be a "good" segment.
+The two segments must be perpendicular to each other.
+The segments must share one cell that is an endpoint of both segments.
+Segments must have length at least 2.
+The length of the longer segment is twice the length of the shorter segment.
+We need to count the number of L-shapes in the grid.
+
+Below you can find two examples of correct L-shapes,
+examples_correct
+
+Examples of valid L-shapes.
+and three examples of invalid L-shapes.
+examples_incorrect
+
+Examples of invalid L-shapes.
+Note that in the shape on the left, two segments do not share a common endpoint. The next two shapes do not meet the last requirement, as in the middle shape both segments have the same length, and in the last shape the longer segment is longer than twice the length of the shorter one.
+
+Input
+The first line of the input contains the number of test cases, T. T test cases follow.
+
+The first line of each testcase contains two integers R and C.
+
+Then, R lines follow, each line with C integers representing the cells of the grid.
+
+Output
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the number of L-shapes.
+
+Input :
+2
+4 3
+1 0 0
+1 0 1
+1 0 0
+1 1 0
+6 4
+1 0 0 0
+1 0 0 1
+1 1 1 1
+1 0 1 0
+1 0 1 0
+1 1 1 0
+
+Output :
+Case # 1: 1
+Case # 2: 9
+*/
+
 package tangible;
 
 import java.io.IOException;
