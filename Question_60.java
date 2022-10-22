7ea0c8bae5469305912d4695ec55d73b89291339
Joydeep Mallick <94801952+JoydeepMallick@users.noreply.github.com>
2022-10-22 08:05:42 +0530

    Update and rename CompetitiveCodingSubArray.java to Question_60.java

--- Question_60.java
+++ Question_60.java
@@ -1,3 +1,25 @@
+/*
+Question 60
+___________
+
+You are given an integer array nums and two integers minK and maxK.
+
+A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+
+The minimum value in the subarray is equal to minK.
+The maximum value in the subarray is equal to maxK.
+Return the number of fixed-bound subarrays.
+
+A subarray is a contiguous part of an array.
+
+Example 1:
+
+Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+Output: 2
+Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+
+*/
+
 public class CompetitiveCodingSubArray {
         public long subArrays(int[] nums, int minK, int maxK) {
             long ans = 0;
